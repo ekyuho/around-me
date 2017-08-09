763e7e95de21a18e84b88775cbffe7ed5ddebdb8
Kyuho Kim <valve.god.project@gmail.com>
2017-08-09 15:05:41 +0900

    Delete thingspeak_template.ino

--- servers/thingspeak_template.ino
+++
@@ -1,8 +0,0 @@
-/*
- * You can use an exmple from ESP8266 Core with two changes.
- * 1. edit ssid and password for your WiFi AP.
- * 2. use your url with no "secure" feature
- */
-
-// WiFiMulti.addAP("SSID", "PASSWORD");
-// http.begin("your own full URL with no secure feature"); //HTTP
